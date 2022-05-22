dbbf15a1cc1361c1239256ada621902ef3291d3c
Thư Trần <anhthu.tran@adaptis.io>
2022-05-22 16:36:29 +0700

    build html landing page

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import mealsImage from '../../assets/meals.jpg';
+
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton';
+
+function Header(props) {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onShowCart={props.onShowCart}/>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A"/>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Header;
